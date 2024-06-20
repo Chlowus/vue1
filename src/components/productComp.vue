7fd7c6b3a7df47efe540aba4b36049a20e638306
Chlowus <chlsmi040904@gmail.com>
2024-06-20 15:28:32 +0200

    m

--- src/components/productComp.vue
+++ src/components/productComp.vue
@@ -1,13 +1,57 @@
-<template lang="">
+<template>
+    <div :style="{
+        backgroundImage: `url(${cover})`,
+        backgroundSize: 'cover',
+        filter: 'blur(4px)',
+    }">
+
+    </div>
+
     <div>
-        <h1>This is a products page</h1>
+        <h1>
+            This is the product component 👍
+            <!-- <br>
+            {{bookTitle.title}} -->
+            <slot name="heading"></slot>
+            <slot name="author"></slot>
+            <slot name="pages"></slot>
+            <slot></slot>
+            <!-- <slot name="publisher"></slot>
+            <slot name="isbn"></slot> -->
+
+            <!--<br>
+            <img :src="bookTitle.cover">
+            {{bookTitle.author}}
+            <br>
+            {{bookTitle.year}}
+            <br>
+            {{bookTitle.genre}}
+            <br>
+            {{bookTitle.pages}}
+            <br>
+            {{bookTitle.publisher}}
+            <br>
+            {{bookTitle.isbn}} -->
+        </h1>
     </div>
 </template>
 <script>
 export default {
-    
+    // props:[
+    // 'bookTitle'
+    // ]
+    props:['cover']
 }
 </script>
-<style lang="">
-    
+<style scoped>
+h1{
+    color: white;
+    text-shadow: 3px 3px black;
+}
+    div{
+        width: 300px;
+        height: 400px;
+        position: absolute;
+        border-radius: 10%;
+    }
 </style>
